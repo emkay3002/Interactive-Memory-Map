05a7b56e7b092dd314db6f22f80b8a2c68c6fa77
cyynira <144375987+cyynira@users.noreply.github.com>
2024-12-08 02:23:50 +0500

    Add files via upload

---
+++ frontend/src/components/dashboard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../stylesheets/dashboard.css"; // Ensure the path is correct
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <div className="dashboard-header">
+        <h2>Groups</h2>
+      </div>
+      <div className="dashboard-content">
+        {/* You can add more content here later */}
+        
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
