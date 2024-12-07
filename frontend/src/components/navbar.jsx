98fd7947bcbe222e4cfbe49be6fc8bc149a68795
emkay3002 <emankhalid2003@gmail.com>
2024-12-07 20:02:36 +0500

    navbar changes

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -5,15 +5,18 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="logo">
-        <h1>My Website</h1>
+        <h1>Moo Deng</h1>
       </div>
       <ul className="nav-links">
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/capsules">Capsules</a></li>
         <li><a href="/profile">Profile</a></li>
-        <li><a href="/login">Login</a></li>
       </ul>
+      <div className="auth-buttons">
+        <a href="/register" className="btn-register">Register</a>
+        <a href="/signin" className="btn-signin">Sign In</a>
+      </div>
     </nav>
   );
 };
--- frontend/src/pages/homepage.jsx
+++ frontend/src/pages/homepage.jsx
@@ -3,16 +3,13 @@ import Navbar from "../components/navbar";
 import "../stylesheets/GradientBackground.css";
 const Homepage = () => {
     return (
+        
       <div className="homepage-container">
         <Navbar /> {}
         <div className="gradient-overlay">
           {}
         </div>
-        <div className="content-container">
-          <h1>Welcome to the Homepage!</h1>
-          <p>Here is your homepage content.</p>
-          {}
-        </div>
+       
       </div>
     );
   };
