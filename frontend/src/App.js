388f14debd157d8879ab9acc738b6c66d40e4ef4
cyynira <144375987+cyynira@users.noreply.github.com>
2024-12-08 17:09:16 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import FriendPage from "./pages/Friends";
 import HomePage from "./pages/homepage";
 import Loginpage from "./pages/loginPage";
 import SignupPage from "./pages/signupPage";
+import Grouppage from "./pages/grouppage";
 function App() {
   return (
     <Router>
@@ -15,6 +16,7 @@ function App() {
         <Route path="/capsules" element={<CapsulePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/friends" element={<FriendPage />} />
+        <Route path ="/grouppage" element={<Grouppage />} />
       </Routes>
     </Router>
   );
