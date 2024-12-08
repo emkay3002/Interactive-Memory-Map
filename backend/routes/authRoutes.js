6f8f5051a59de24514aabd531b7008231b15f553
cyynira <144375987+cyynira@users.noreply.github.com>
2024-12-08 17:03:40 +0500

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,11 +2,17 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
-const { signup, login } = require("../controllers/authController");
+
+const { signup, login, createGroup,getUserGroups, saveRoles } = require("../controllers/authController");
 const router = express.Router();
 
+
+
+// In authRoutes.js
+router.get("/user-groups", getUserGroups);
 router.post("/signup", signup);
 router.post("/login", login);
-//router.post("/signup", signup);
+router.post("/create-group", createGroup);
+router.put("/groups/:groupId/roles", saveRoles);
 
 module.exports = router;
