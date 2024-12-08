771e1e08a89323c8cc08e29ee688a8cb269ee056
cyynira <144375987+cyynira@users.noreply.github.com>
2024-12-08 17:08:06 +0500

    Update grouppage.jsx

--- frontend/src/pages/grouppage.jsx
+++ frontend/src/pages/grouppage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../components/navbar";
+import GenNavbar from "../components/GenNavbar"
 import Sidebar from "../components/sidebar";
 import Dashboard from "../components/dashboard"; // Import the Dashboard component
 import "../stylesheets/GradientBackground.css"; // Ensure correct path
@@ -8,7 +9,7 @@ import "../stylesheets/groupPanel.css"; // Ensure correct path
 const Grouppage = () => {
   return (
     <div className="homepage-container">
-      <Navbar />
+      <GenNavbar />
       
 
       <div className="content-container">
@@ -23,12 +24,7 @@ const Grouppage = () => {
       <div className="gradient-overlay">
         
       </div>
-      <img 
-        id="image" 
-        src="https://img.icons8.com/?size=100&id=Aim4pBWMKPgj&format=png&color=7950F2" 
-        style={{ width: '100px', height: '100px', margin: '50px' }} 
-        alt="Icon" 
-        />
+      
 
     </div>
     
